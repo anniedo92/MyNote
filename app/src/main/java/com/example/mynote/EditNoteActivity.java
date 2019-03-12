e60d6797a8290a8cbac01e7fbc70974f47f11c9a
John Hwang <johnhwang77@gmail.com>
2019-03-12 05:56:18 -0400

    added editnoteactivity in manifest, added some stuff to editnoteactivity and xml

--- app/src/main/java/com/example/mynote/EditNoteActivity.java
+++ app/src/main/java/com/example/mynote/EditNoteActivity.java
@@ -3,15 +3,20 @@ package com.example.mynote;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditNoteActivity extends AppCompatActivity {
     private EditText editText;
     private Button btnSave;
     private Button btnCancel;
     private Note note;
+    private TextView textDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,11 @@ public class EditNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_note);
 
         this.editText = (EditText) findViewById(R.id.editTitle);
-        this.btnSave = (Button) findViewById(R.id.btnSave);
         this.btnCancel = (Button) findViewById(R.id.btnCancel);
         this.editText = (EditText) findViewById(R.id.editFullText);
 
         note = new Note();
+
         initSaveBtn();
 
         Bundle bundle = getIntent().getExtras();
@@ -33,50 +38,68 @@ public class EditNoteActivity extends AppCompatActivity {
                     this.editText.setText(note.getFullText());
                 }
             }
-        }
 
-        private void initSaveBtn() {
+        //John Hwang below
+
+        /*Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+
+        } else {
+            note = new Note();
+        }
+        note = new Note();
+
+        textDate = findViewById(R.id.textDate);
+        textDate.setText("Created on: " + note.getDate());
+
+        setEditing(true);
+        initChangedText();*/
+        setEditing(true);
+        initChangedText();
+    }
+
+    private void initSaveBtn() {
 
             Button btnSave = (Button) findViewById(R.id.btnSave);
             btnSave.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View v) {
-                    boolean success = false;
-                    NoteDataSource ds = new NoteDataSource(EditNoteActivity.this);
-                    try {
-                        ds.open();
+            @Override
+            public void onClick(View v) {
+                boolean success = false;
+                NoteDataSource ds = new NoteDataSource(EditNoteActivity.this);
+                try {
+                    ds.open();
 
-                        if (note.getNoteId()== -1) {
-                            success = ds.insertNote(note);
-                            int newId = ds.getLastNoteId();
-                            note.setNoteId(newId);
-                        } else{
-                            success = ds.updateNote(note);
-                        }
-                        ds.close();
+                    if (note.getNoteId()== -1) {
+                        success = ds.insertNote(note);
+                        int newId = ds.getLastNoteId();
+                        note.setNoteId(newId);
+                    } else{
+                        success = ds.updateNote(note);
                     }
-                    catch (Exception e) {
-                        success = false;
-                    }
-                    if (success) {
-                        Intent intent = new Intent(EditNoteActivity.this, NoteActivity.class);
-                        startActivity(intent);
-                    }
-
+                    ds.close();
                 }
-            });
-        }
-
-        private void initCancelBtn() {
-            Button btnCancel = (Button) findViewById(R.id.btnCancel);
-            btnCancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
+                catch (Exception e) {
+                    success = false;
                 }
-            });
-        }
+                if (success) {
+                    Intent intent = new Intent(EditNoteActivity.this, NoteActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    private void initCancelBtn() {
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
 //        private void initSaveButton() {
 //        Button saveButton = (Button) findViewById(R.id.buttonSave);
@@ -139,4 +162,55 @@ public class EditNoteActivity extends AppCompatActivity {
 //            this.finish();
 //        }
 
+
+    public void initNote(int noteId) {
+
+    }
+
+    private void setEditing(boolean enabled) {
+        EditText editTitle = findViewById(R.id.editTitle);
+        EditText editBody = findViewById(R.id.editFullText);
+
+        editTitle.setEnabled(enabled);
+        editBody.setEnabled(enabled);
+    }
+
+    private void initChangedText() {
+        final EditText editTitle = findViewById(R.id.editTitle);
+        final EditText editBody = findViewById(R.id.editFullText);
+
+        editTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                note.setFullText(editTitle.getText().toString());
+            }
+        });
+
+        editBody.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                note.setFullText(editBody.getText().toString());
+            }
+        });
+    }
 }
